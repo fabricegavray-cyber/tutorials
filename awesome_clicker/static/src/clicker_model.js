ea55d05cb7d79f88dfc3d323b0f404cd6b981a9c
Fabrice Gavray <fabrice.gavray@gmail.com>
2026-05-08 10:31:26 +0200

    master cahpter 2 ex 10 done

--- awesome_clicker/static/src/clicker_model.js
+++ awesome_clicker/static/src/clicker_model.js
@@ -1,10 +1,14 @@
 /** @odoo-module **/
 import { Reactive } from "@web/core/utils/reactive";
+import { EventBus } from "@odoo/owl";
 
 export class ClickerModel extends Reactive {
     clicks = 0;
     level = 0;
     clickBots = 0;
+
+    bus = new EventBus();
+    _milestone1kReached = false;
 
     increment(inc = 1) {
         this.clicks += inc;
@@ -12,8 +16,11 @@ export class ClickerModel extends Reactive {
     }
 
     checkLevel() {
-        if (this.clicks >= 1000 && this.level < 1) {
+        if (this.clicks >= 1000 && !this._milestone1kReached) {
+            this._milestone1kReached = true;
             this.level = 1;
+            // Le modèle émet un événement, sans connaître l'UI
+            this.bus.trigger("MILESTONE_1K");
         }
     }
 
--- awesome_clicker/static/src/clicker_service.js
+++ awesome_clicker/static/src/clicker_service.js
@@ -1,13 +1,20 @@
 /** @odoo-module **/
 import { registry } from "@web/core/registry";
-import { reactive } from "@odoo/owl";
 import { useState } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 import { ClickerModel } from "./clicker_model";
 
 const clickerService = {
-    start() {
+    dependencies: ["effect"],  
+    start(env, { effect }) {   
         const model = new ClickerModel();
+
+        model.bus.addEventListener("MILESTONE_1K", () => {
+            effect.add({
+                type: "rainbow_man",
+                message: "1000 clics ! Vous pouvez maintenant acheter des ClickBots !",
+            });
+        });
 
         setInterval(() => model.tick(), 10000);
 
@@ -16,7 +23,7 @@ const clickerService = {
 };
 
 export function useClicker() {
-     return useState(useService("clicker"));
+    return useState(useService("clicker"));
 }
 
 registry.category("services").add("clicker", clickerService);
